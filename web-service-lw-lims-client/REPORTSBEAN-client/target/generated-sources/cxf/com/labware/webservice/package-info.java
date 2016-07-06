6a71f51c30ab1df6808563ec250cde2d9b42d6a8
gdeignacio <gdeignacio@gmail.com>
2016-07-06 18:56:08 +0200

    gitignore

--- web-service-lw-lims-client/REPORTSBEAN-client/target/generated-sources/cxf/com/labware/webservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.labware.com/webservice", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.labware.webservice;
--- web-service-lw-lims-client/labware-weblims-authenticate-client/target/generated-sources/cxf/labware_weblims_authenticate/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "labware_weblims_authenticate", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package labware_weblims_authenticate;
